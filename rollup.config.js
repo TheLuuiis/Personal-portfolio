49035420bdba5f5fbbbf1fceeea14d5fe5aeb080
TheLuis <luisdavidgilcabarcas47@gmail.com>
2025-02-21 10:16:05 -0500

    Instalación de plugings

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import sass from 'rollup-plugin-sass';
+
 export default {
 	input: 'src/index.js',
 	output: {
 		file: 'public/bundle.js',
 		format: 'cjs',
 	},
+	plugins: [
+		sass({
+		  output: 'public/bundle.css'
+		})
+	  ]
 };
